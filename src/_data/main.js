c4b93169871d5627a483197928110e461556fc0d
alpha14 <contact@alpha14.com>
2020-10-18 01:21:44 +0200

    fix: Move images to local repo

--- src/_data/main.js
+++ src/_data/main.js
@@ -9,21 +9,21 @@ module.exports = {
     publications: "Publications",
     carousel: [
       {
-        img: "https://images.unsplash.com/photo-1575761344173-aa186491c120?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        img: "../assets/images/photo-1575761344173.webp",
         title: "Who are we ?",
         subtitle: "Altertek is an association whose aim is to offer alternatives in terms of the use and design of Information and Communication Technologies to preserve the environment and our resources",
         button: "Learn more",
         link: ""
       },
       {
-        img: "https://images.unsplash.com/photo-1440964829947-ca3277bd37f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        img: "../assets/images/photo-1440964829947.webp",
         title: "MEET. Our alternative to zoom",
         subtitle: "Discover our videoconference instance (based on jitsi), to discuss with your friends, your students or your community",
         button: "Try MEET",
         link: ""
       },
       {
-        img: "https://images.unsplash.com/photo-1540655037529-dec987208707?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1394&q=80",
+        img: "../assets/images/photo-1540655037529.webp",
         title: "Simple Videos",
         subtitle: "Reduce my resource consumption when watching YouTube",
         button: "Download the plugin",
@@ -63,30 +63,29 @@ module.exports = {
     publications: "Nos publications",
     carousel: [
       {
-        img: "https://images.unsplash.com/photo-1575761344173-aa186491c120?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        img: "../assets/photo-1575761344173.webp",
         title: "Qui sommes-nous ?",
         subtitle: "Altertek est une association ayant pour but de proposer des alternatives en terme d’usage et de conception des Technologies de l’Information et de la Communication pour préserver l’environnement et nos ressources.",
         button: "En savoir plus",
         link: ""
       },
       {
-        img: "https://images.unsplash.com/photo-1440964829947-ca3277bd37f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        img: "../assets/images/photo-1440964829947.webp",
         title: "MEET. Notre alternative à zoom",
         subtitle: "Découvrez notre instance de visio-conférence (basée sur jitsi), pour échanger avec vos amis, vos élèves ou votre communauté.",
         button: "Essayer MEET",
         link: ""
       },
       {
-        img: "https://images.unsplash.com/photo-1540655037529-dec987208707?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1394&q=80",
-        title: "Simple Vidéos",
-        subtitle: "Reduire ma consommation de ressources quand je regarde youtube",
+        img:  "../assets/photo-1540655037529.webp",
+        subtitle:  "Reduire ma consommation de ressources quand je regarde youtube",
         button: "Télécharger le plugin",
         link: ""
       }
     ],
     showcase: {
       title: "De l’éthique dans la technologie",
-      subtitle: "Altertek est une association ayant pour but de proposer des alternatives en terme d’usage et de conception des Technologies de l’Information et de la Communication pour préserver l’environnement et nos ressources.",
+      subtitle: "Altertek est une association ay ant pour but de proposer des alternatives en terme d’usage et de conception des Technologies de l’Information et de la Communication pour préserver l’environnement et nos ressources.",
       columns: [
         {
           icon: "book",
