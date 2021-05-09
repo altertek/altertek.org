37cf38109b6076904c21582781aadbc62e83ab5f
saxjst <51910006+saxjst@users.noreply.github.com>
2021-05-09 19:29:04 +0200

    feat: add associations in "we support" section

--- src/_data/main.js
+++ src/_data/main.js
@@ -70,9 +70,29 @@ module.exports = {
       title: "We support",
       refs: [
         {
+          name: "Zero Waste France",
+          img: "../../assets/images/references/zerowaste.webp",
+          link: "https://www.zerowastefrance.org"
+        },
+        {
+          name: "ANV COP21",
+          img: "../../assets/images/references/anv-cop21.webp",
+          link: "https://anv-cop21.org"
+        },
+        {
+          name: "Alternatiba",
+          img: "../../assets/images/references/alternatiba.webp",
+          link: "https://alternatiba.eu"
+        },
+        {
           name: "World CleanUp Day – France",
           img: "../../assets/images/references/word-cleanup-day.webp",
           link: "https://www.worldcleanupday.fr"
+        },
+        {
+          name: "Cyber World CleanUp Day",
+          img: "../../assets/images/references/cwud.webp",
+          link: "https://cyberworldcleanupday.fr"
         }
       ]
     }
@@ -143,9 +163,29 @@ module.exports = {
       title: "Nous soutenons",
       refs: [
         {
+          name: "Zero Waste France",
+          img: "../../assets/images/references/zerowaste.webp",
+          link: "https://www.zerowastefrance.org"
+        },
+        {
+          name: "ANV COP21",
+          img: "../../assets/images/references/anv-cop21.webp",
+          link: "https://anv-cop21.org"
+        },
+        {
+          name: "Alternatiba",
+          img: "../../assets/images/references/alternatiba.webp",
+          link: "https://alternatiba.eu"
+        },
+        {
           name: "World CleanUp Day – France",
           img: "../../assets/images/references/word-cleanup-day.webp",
           link: "https://www.worldcleanupday.fr"
+        },
+        {
+          name: "Cyber World CleanUp Day",
+          img: "../../assets/images/references/cwud.webp",
+          link: "https://cyberworldcleanupday.fr"
         }
       ]
     }
