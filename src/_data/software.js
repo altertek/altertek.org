ce170a7eb9a6227697eaeb03ca684552dbe00a7e
alpha14 <alpha14@users.noreply.github.com>
2022-08-09 22:14:59 +0200

    feat: add wifiqr

--- src/_data/software.js
+++ src/_data/software.js
@@ -1,6 +1,13 @@
 module.exports = {
   en: {
     software: [{
+      icon: "wifi",
+      title: "WiFi QR",
+      subtitle: "WiFi credential sharing",
+      button: "See the project",
+      link: "https://github.com/altertek/wifiqr"
+    },
+    {
       img: "../../assets/images/software/whatimupto.png",
       title: "Whatimupto",
       subtitle: "Linktree alternative",
@@ -11,11 +18,19 @@ module.exports = {
   },
   fr: {
     software: [{
+      icon: "wifi",
+      title: "Wifi QR",
+      subtitle: "Partage d'identifiants WiFi",
+      button: "Voir le projet",
+      link: "https://github.com/altertek/wifiqr"
+    },
+    {
       img: "../../assets/images/software/whatimupto.png",
       title: "Whatimupto",
       subtitle: "Alternative à Linktree",
       button: "Voir le projet",
       link: "https://github.com/altertek/whatimupto"
     }]
+
   }
 }
